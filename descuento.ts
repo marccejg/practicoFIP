7dfc0880fee29d7cd1898a7ee204605d425093f8
Marcelo Javier García <marcejgarcia@gmail.com>
2024-10-17 20:39:30 -0300

    Update descuento.ts

--- descuento.ts
+++ descuento.ts
@@ -1,10 +1,6 @@
-/**Implemente un algoritmo que calcule y muestre
-por pantalla el precio final de un producto
-después de aplicarle un descuento
+/**
 • El precio inicial del producto se ingresa por pantalla
-• El descuento a aplicar es del 10%. Recuerde que
-puede obtener el 10% de un valor multiplicado por 0,1
-• Precio final debe ser mostrado en pantalla 
+
 Alumnos:
   Diana Ciganda
   Pamela Tolosa
@@ -12,7 +8,6 @@ Alumnos:
 */
 
 import * as rs from "readline-sync";
-const DESCUENTO: number = 1.5;
 const MES_DESCUENTO: number = 10;
 let producto: string = rs.question("ingrese producto de ceramica que desea comprar:  ");
 let precio_unitario: number = rs.questionFloat("ingrese precio por unidad: ");
